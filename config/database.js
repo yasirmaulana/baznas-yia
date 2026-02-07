ba4a1859a890e4d2318584f85a4d21631a6b660f
yasirmaulana <yasir.maulana@gmail.com>
2026-02-07 17:55:10 +0700

    update config db

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
+const pg = require('pg');
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 // PostgreSQL connection using DATABASE_URL
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectModule: pg,
     protocol: 'postgres',
     dialectOptions: {
         ssl: {
